f1a51193cc8dc04683b82e93138d73699f81b43b
Josgar123 <122987191+Josgar123@users.noreply.github.com>
2024-12-02 15:50:26 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,39 +1,22 @@
-<!doctype html>
+  <!DOCTYPE html>
 
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bootstrap demo</title>
-    <link href=https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
   </head>
 
   <body>
 
-    <div class="container">
-      
-        <h1>Welcome to MIS4013hw5</h1>
-          
-           <ul class="nav nav-tabs">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="index.php"">Page 1</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="page2.php">Page 2</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="page3.php"">Page 3</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="page4.php"">Page 4</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="page5.php"">Page 5</a>
-              </li>
-            </ul>
-
-      </div>
-    
+      <nav>
+        <a  href="index.php">Page 1</a>|
+        <a  href="page2.php">Page 2</a>|
+        <a  href="page3.php">Page 3</a>|
+        <a  href="page4.php">Page 4</a>|
+        <a  href="page5.php">Page 5</a>
+        
+      </nav>
+  
       <script>
           function showMessage() {
               alert("Want to know more about our pages?");
