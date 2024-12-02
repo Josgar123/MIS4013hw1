a46e12604bb4c78c92058b87c3af18dce909e6e2
Josgar123 <122987191+Josgar123@users.noreply.github.com>
2024-12-02 16:03:49 -0600

    Update page2.php

--- page2.php
+++ page2.php
@@ -1,56 +1,49 @@
+
 <!doctype html>
 
 <html lang="en">
-
   <head>
-
     <meta charset="utf-8">
-
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <title>Bootstrap demo</title>
-
     <link href=https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-
   </head>
 
   <body>
 
     <div class="container">
-
-    <h1>Welcome to MIS4013hw1</h1>
       
-   <ul class="nav nav-tabs">
-      <li class="nav-item">
-        <a class="nav-link"  href="index.php">Page 1</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link active" aria-current="page" href="page2.php">Page 2</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="page3.php">Page 3</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="page4.php">Page 4</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="page5.php">Page 5</a>
-      </li>
-    </ul>
-  </br>
+        <h1>Welcome to MIS4013hw5</h1>
+          
+           <ul class="nav nav-tabs">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="index.php"">Page 1</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="page2.php">Page 2</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="page3.php"">Page 3</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="page4.php"">Page 4</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="page5.php"">Page 5</a>
+              </li>
+            </ul>
+
+      <button onclick = "showMessage()"> "Click Me" </button>
+      <script>
+          // Change heading:
+document.getElementById("Movie Name").innerHTML = "Movie Name";
+
+// Change paragraph:
+document.getElementById("Favorite").innerHTML = "Favorite Range";
+      </script>
+      </div>
+
     
-   <ul class="list-group">
-  <li class="list-group-item active" aria-current="true">Movies</li>
-  <li class="list-group-item">Food</li>
-  <li class="list-group-item">Customers</li>
-  <li class="list-group-item"> Tickets</li>
-  <li class="list-group-item">Theaters</li>
-</ul>
-      
-    <script src=https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
-
-  </div>
-
   </body>
 
 </html>
