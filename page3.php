5eb5479b7677e7f5d88331b4b9554a79e505c62c
Josgar123 <122987191+Josgar123@users.noreply.github.com>
2024-12-02 16:23:52 -0600

    Update page3.php

--- page3.php
+++ page3.php
@@ -2,73 +2,46 @@
 <!doctype html>
 
 <html lang="en">
-
   <head>
-
     <meta charset="utf-8">
-
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <title>Bootstrap demo</title>
-
     <link href=https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-
   </head>
 
   <body>
 
     <div class="container">
-
-    <h1>Welcome to MIS4013hw1</h1>
       
-   <ul class="nav nav-tabs">
-      <li class="nav-item">
-        <a class="nav-link" href="index.php">Page 1</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="page2.php">Page 2</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link active" aria-current="page" href="page3.php">Page 3</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="page4.php">Page 4</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="page5.html">Page 5</a>
-      </li>
-    </ul>
-  </br>
+        <h1>Welcome to MIS4013hw5</h1>
+          
+           <ul class="nav nav-tabs">
+              <li class="nav-item">
+                <a class="nav-link " href="index.php"">Page 1</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="page2.php">Page 2</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="page3.php"">Page 3</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="page4.php"">Page 4</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="page5.php"">Page 5</a>
+              </li>
+            </ul>
+
+      <button onclick = "getLocation()"> "Click Me" </button>
+      <script>
+        function getLocation() 
+        { if (navigator.geolocation) { navigator.geolocation.getCurrentPosition(showPosition); } 
+        else { myElement.innerHTML = "Movies are fun."; } }
+      </script>
+      </div>
+
     
-    <div class="card">
-  <div class="card-body">
-    Theaters.
-  </div>
-</div>
-      <div class="card">
-  <div class="card-body">
-    Favorite movies.
-  </div>
-</div>
-      <div class="card">
-  <div class="card-body">
-    Favorite foods.
-  </div>
-</div>
-      <div class="card">
-  <div class="card-body">
-    Customers.
-  </div>
-</div>
-      <div class="card">
-  <div class="card-body">
-    Tickets.
-  </div>
-</div>
-    <script src=https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
-
-  </div>
-
   </body>
 
 </html>
