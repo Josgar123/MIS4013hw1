a470e17aa8909f28cf44e50bda158738afe7045d
Josgar123 <122987191+Josgar123@users.noreply.github.com>
2024-12-02 21:15:43 -0600

    Update page4.php

--- page4.php
+++ page4.php
@@ -2,58 +2,46 @@
 <!doctype html>
 
 <html lang="en">
-
   <head>
-
     <meta charset="utf-8">
-
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <title>Bootstrap demo</title>
-
     <link href=https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-
   </head>
 
   <body>
 
     <div class="container">
-
-    <h1>Welcome to MIS4013hw1</h1>
       
-   <ul class="nav nav-tabs">
-      <li class="nav-item">
-        <a class="nav-link " href="index.php">Page 1</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="page2.php">Page 2</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="page3.php">Page 3</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link active" aria-current="page" href="page4.php">Page 4</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="page5.php">Page 5</a>
-      </li>
-    </ul>
-  </br>
+        <h1>Welcome to MIS4013hw5</h1>
+          
+           <ul class="nav nav-tabs">
+              <li class="nav-item">
+                <a class="nav-link " href="index.php"">Page 1</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="page2.php">Page 2</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="page3.php"">Page 3</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="page4.php"">Page 4</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="page5.php"">Page 5</a>
+              </li>
+            </ul>
+
+     <button onclick="myFunction()">Go Back</button>
+      <script>
+function myFunction() {
+  window.history.back();
+}
+</script>
+      </div>
+
     
-<button type="button" class="btn btn-primary">Movies</button>
-<button type="button" class="btn btn-secondary">Foods</button>
-<button type="button" class="btn btn-success">Theaters</button>
-<button type="button" class="btn btn-danger">Customers</button>
-<button type="button" class="btn btn-warning">Tickets</button>
-
-
-<button type="button" class="btn btn-link">Link</button>
-      
-
-    <script src=https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
-
-  </div>
-
   </body>
 
 </html>
